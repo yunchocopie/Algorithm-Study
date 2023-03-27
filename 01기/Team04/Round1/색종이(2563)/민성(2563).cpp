c25e7412ad6c890e20fcc58a08ca57d163e99671
Utoopia <122884371+Utoopia@users.noreply.github.com>
2023-03-27 18:32:41 +0900

    Update 민성(2563).cpp

--- 01기/Team04/Round1/색종이(2563)/민성(2563).cpp
+++ 01기/Team04/Round1/색종이(2563)/민성(2563).cpp
@@ -5,27 +5,62 @@
 #define endl '\n'
 using namespace std;
 
-vector <pair<int, int>> v;
+int paint[100][100] = { 0 };
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
-    int paper, garo, sero;
+    int paper, garo, sero, area = 0;
+
+    cin >> paper;
 
     for (int i = 0; i < paper; i++) {
         cin >> garo >> sero;
-        v.push_back(make_pair(garo, sero));
-    }
-
-    sort(v.begin(), v.end());
-
-    for (int i = 0; i < paper; i++) {
-        for (int j = 0; j < paper; j++) {
-            if
+        for (int j = garo; j < garo + 10; j++) {
+            for (int k = sero; k < sero + 10; k++) {
+                if (paint[j][k] == 0) { 
+                    paint[j][k] = 1;
+                    area++;
+                }
+            }
         }
+
     }
+
+    cout << area;
 
     return 0;
 }
+
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(0);
+//    cout.tie(0);
+//
+//    int paper, garo, sero, area = 0;
+//
+//    cin >> paper;
+//
+//    for (int i = 0; i < paper; i++) {
+//        cin >> garo >> sero;
+//        v.push_back(make_pair(garo, sero));
+//    }
+//
+//    sort(v.begin(), v.end());
+//
+//    for (int i = 0; i < paper; i++) {
+//        for (int j = i + 1; j < paper; j++) {
+//            if (v[j].first - v[i].first < 10) {
+//                garo = (v[i].first + 10) - v[j].first;
+//                sero = (v[j].second + 10) - v[i].second;
+//                area += garo * sero;
+//            }
+//        }
+//    }
+//
+//    cout << paper * 100 - area;
+//
+//    return 0;
+//}
