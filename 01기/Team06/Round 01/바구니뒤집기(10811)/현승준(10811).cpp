e6230987eb7a7aee0eeb3eeb37cee7613ae35b81
현승준 <113924801+Comet-Csms21@users.noreply.github.com>
2023-04-02 06:46:05 +0900

    Modify 현승준(10811).cpp

--- 01기/Team06/Round 01/바구니뒤집기(10811)/현승준(10811).cpp
+++ 01기/Team06/Round 01/바구니뒤집기(10811)/현승준(10811).cpp
@@ -1,45 +1,33 @@
 #include <iostream>
-#include <fstream>
 #include <algorithm>
+#define endl '\n'
 using namespace std;
 
 int main()
 {
-    /*ifstream ifs;
-    ifs.open("input.txt");
-    if (ifs.fail())
-    {
-        cerr << "Error!" << endl;
-        return -1;
-    }
-
-    ofstream ofs;
-    ofs.open("output.txt");
-    if (ofs.fail())
-    {
-        cerr << "Error!" << endl;
-        return -1;
-    }*/
-
+    // 바구니 N개(1~N) M번 뒤집기
     int N, M;
 
-    //ifs >> N >> M;
     cin >> N >> M;
 
+    // 바구니 수 만큼 배열 생성
     int* arr = new int[N];
 
+    // 바구니 숫자 부여
     for (int i = 0; i < N; i++) {
         arr[i] = i + 1;
     }
 
+    // from바구니부터 to바구니까지 뒤집기
     int from, to;
 
+    // M번 반복해서 뒤집기
     for (int i = 0; i < M; i++) {
-        //ifs >> from >> to;
         cin >> from >> to;
 
         reverse(arr + (from - 1), arr + to);
 
+        // 바구니가 잘 뒤집어지는지 확인용
         /*for (int i = 0; i < N; i++) {
             cout << arr[i];
         }
@@ -47,16 +35,11 @@ int main()
     }
 
     for (int i = 0; i < N; i++) {
-        //ofs << arr[i];
         cout << arr[i];
         if (i == N - 1)
             break;
-        //ofs << ' ';
         cout << ' ';
     }
-
-    //ifs.close();
-    //ofs.close();
 
     return 0;
 }
